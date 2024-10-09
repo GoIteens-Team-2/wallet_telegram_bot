20a0d9c633b97e70665b7a76a3308ec17ac2c328
savenkovsemen02@gmail.com <savenkovsemen02@gmail.com>
2024-10-09 19:26:12 +0300

    update database

--- app.py
+++ app.py
@@ -1,15 +1,37 @@
 import json
+import asyncio
+from config import token
+from aiogram import Dispatcher, Bot, types
+from aiogram.filters import CommandStart
 
+dp = Dispatcher()
+bot = Bot(token="TOKEN")
 
-def write_inf(data, file_name):
-    data = json.dumps(data)
-    data = json.loads(str(data))
+@dp.message(CommandStart())
+async def commandstart(message: types.Message):
+    def write_inf(data, file_name):
+        data = json.dumps(data)
+        data = json.loads(str(data))
 
-    with open(file_name, 'w', encoding="utf-8") as file:
-        json.dump(data, file, indent=4)
+        with open(file_name, 'w', encoding="utf-8") as file:
+            json.dump(data, file, indent=4)
 
-data = {
-    "name" : "sorrow"
-}
+    trunsuction = "path_to_file.json"
 
-write_inf(data, "user_info")
+    data = {
+        "trunsuction": []
+    }
+
+    data["trunsuction"].append(
+        {
+            "name": f"{trunsuction}"
+        }
+    )
+
+    write_inf(data, trunsuction)
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
