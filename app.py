2ea90db3b5dfadbfa4205981b1ae9a10164e0861
savenkovsemen02@gmail.com <savenkovsemen02@gmail.com>
2024-10-09 18:37:30 +0300

    create database

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print("team 2")
+import json
+
+
+def write_inf(data, file_name):
+    data = json.dumps(data)
+    data = json.loads(str(data))
+
+    with open(file_name, 'w', encoding="unt-8") as file:
+        json.dump(data, file, indent=4)
+
+data = {
+    "name" : "sorrow"
+}
+
+write_inf(data, "user_info")
