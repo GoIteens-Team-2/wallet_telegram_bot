d2ec9c13a08dc23bdca327303b62e2c1b1d7870e
savenkovsemen02@gmail.com <savenkovsemen02@gmail.com>
2024-10-09 18:41:32 +0300

    update database

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ def write_inf(data, file_name):
     data = json.dumps(data)
     data = json.loads(str(data))
 
-    with open(file_name, 'w', encoding="unt-8") as file:
+    with open(file_name, 'w', encoding="utf-8") as file:
         json.dump(data, file, indent=4)
 
 data = {
