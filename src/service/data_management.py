8c9facf011b5c83d21e7ab1290422a90d38cb590
savenkovsemen02@gmail.com <savenkovsemen02@gmail.com>
2024-10-17 20:23:24 +0300

    all transaction files are now saved in a separate folder

--- src/service/data_management.py
+++ src/service/data_management.py
@@ -1,11 +1,14 @@
 import json
+import os
 
 class DataManager:
-    def init(self) -> None:
+    def __init__(self) -> None:
         self.user_data = {}
+        if not os.path.exists("users_transactions"):
+            os.makedirs("users_transactions")
 
     def load_user_data(self, user_id):
-        file_name = f"data/user_{user_id}_data.json"
+        file_name = f"users_transactions/user_{user_id}_data.json"
         try:
             with open(file_name, 'r', encoding="utf-8") as file:
                 self.user_data[user_id] = json.load(file)
@@ -13,9 +16,8 @@ class DataManager:
             self.user_data[user_id] = {"balance": 0, "transactions": []}
 
     def save_user_data(self, user_id):
-        file_name = f"data/user_{user_id}_data.json"
+        file_name = f"users_transactions/user_{user_id}_data.json"
         with open(file_name, 'w', encoding="utf-8") as file:
             json.dump(self.user_data[user_id], file, indent=4)
-
 
 data_manager = DataManager()
