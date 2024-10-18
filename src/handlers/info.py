7d1dc9a5f624863f7921816ff56cfc85c471c5a5
Риманов Ростислав <r.rostislav2019@gmail.com>
2024-10-18 22:21:05 +0300

    Update info.py
    Додав Кнопки

--- src/handlers/info.py
+++ src/handlers/info.py
@@ -1,19 +1,29 @@
 from aiogram import Router, F
 from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.types import Message, ReplyKeyboard, KeyboardButton
 
 from ..service.data_management import data_manager
 
 info_router = Router()
+
+def main_menu_keyboard():
+    keyboard=
+    ReplyKeyBoardMarkup(resize_keyboard=True) 
+    keyboard.add(KeyboardButton("Прибуток"), 
+    KeyboardButton("Витрати")) 
+    keyboard.add(KeyboardButton("Мій баланс"), 
+    KeyboardButton("Що таке wallet-bot?")) 
+return keyboard
+    
 
 @info_router.message(CommandStart())
 async def commandstart(message: Message):
     user_id = message.from_user.id
     data_manager.load_user_data(user_id) 
     welcome_message = (
-        f"Вітаємо, якщо потрібна допомога по боту впишіть команду '/help'"
+        f"Вітаємо, якщо потрібна допомога по боту впишіть команду '/help'""Ось доступні опції:"
     )
-    await message.answer(welcome_message)
+    await message.answer(welcome_message,reply_markup=main_menu_keyboared())
 
 @info_router.message(F.text == "Прибуток")
 async def request_income_info(message: Message):
@@ -24,7 +34,7 @@ async def request_expense_info(message: Message):
     await message.answer("Будь ласка, введіть витрату у форматі: /expense {сума} {опис}")
 
 
-@info_router.message(F.text == "мій баланс")
+@info_router.message(F.text == "Мій баланс")
 async def show_balance(message: Message):
     user_id = message.from_user.id
     data_manager.load_user_data(user_id)
