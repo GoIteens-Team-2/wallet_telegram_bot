39676432195d48466c7b42cf7d432507eef77359
Риманов Ростислав <r.rostislav2019@gmail.com>
2024-10-18 23:12:50 +0300

    Update buttons.py

--- src/service/buttons.py
+++ src/service/buttons.py
@@ -1,1 +1,12 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+def main_menu_keyboard():
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(KeyboardButton("Прибуток"), KeyboardButton("Витрати"))
+    keyboard.add(KeyboardButton("Мій баланс"), KeyboardButton("Що таке wallet-bot?"))
+    return keyboard
+
+def transaction_menu_keyboard():
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(KeyboardButton("Історія транзакцій"))
+    return keyboard
