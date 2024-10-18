f82f988a8d812169cd801d074c75e8360cca9c77
savenkovsemen02@gmail.com <savenkovsemen02@gmail.com>
2024-10-18 21:00:02 +0300

    create command '/help'

--- src/handlers/info.py
+++ src/handlers/info.py
@@ -11,12 +11,7 @@ async def commandstart(message: Message):
     user_id = message.from_user.id
     data_manager.load_user_data(user_id)
     welcome_message = (
-        f"Вітаємо, {message.from_user.first_name}!\n"
-        f"Ваш поточний баланс: {data_manager.user_data[user_id]['balance']} грн.\n"
-        f"Ось доступні команди:\n"
-        f"/income {{сума}} {{опис}} - Додати дохід\n"
-        f"/expense {{сума}} {{опис}} - Додати витрату\n"
-        f"/history - Переглянути історію транзакцій"
+        f"Вітаємо, якщо потрібна допомога по боту впишіть команду '/help'"
     )
     await message.answer(welcome_message)
 
@@ -29,8 +24,22 @@ async def request_expense_info(message: Message):
     await message.answer("Будь ласка, введіть витрату у форматі: /expense {сума} {опис}")
 
 
-@info_router.message(F.text == "Ваш баланс")
+@info_router.message(F.text == "мій баланс")
 async def show_balance(message: Message):
     user_id = message.from_user.id
     data_manager.load_user_data(user_id)
     await message.answer(f"Ваш поточний баланс: {data_manager.user_data[user_id]['balance']} грн.")
+
+
+@info_router.message(F.text == "/help")
+async def show_balance(message: Message):
+    user_id = message.from_user.id
+    data_manager.load_user_data(user_id)
+    help_message = (
+        f"Ваш поточний баланс: {data_manager.user_data[user_id]['balance']} грн.\n"
+        f"Ось доступні команди:\n"
+        f"/income {{сума}} {{опис}} - Додати дохід\n"
+        f"/expense {{сума}} {{опис}} - Додати витрату\n"
+        f"/history - Переглянути історію транзакцій"
+    )
+    await message.answer(help_message)    
