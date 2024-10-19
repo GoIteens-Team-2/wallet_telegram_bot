bfd2ea3fca0262cb06c59b17649b59ad020569ce
savenkovsemen02@gmail.com <savenkovsemen02@gmail.com>
2024-10-19 12:41:02 +0300

    upgraded code

--- src/handlers/info.py
+++ src/handlers/info.py
@@ -1,8 +1,11 @@
 from aiogram import Router, F
 from aiogram.filters import CommandStart
-from aiogram.types import Message, ReplyKeyboard, KeyboardButton
+from aiogram.types import Message
 from ..service.data_management import data_manager
-from ..service.buttons import main_menu_keyboard
+from ..keyboards.buttons import main_menu_keyboard
+
+
+info_router = Router()
 
 @info_router.message(CommandStart())
 async def commandstart(message: Message):
@@ -13,20 +16,11 @@ async def commandstart(message: Message):
     )
     await message.answer(welcome_message, reply_markup=main_menu_keyboard()) 
 
-info_router = Router()
 
-def main_menu_keyboard():
-    keyboard=
-    ReplyKeyBoardMarkup(resize_keyboard=True) 
-    keyboard.add(KeyboardButton("Прибуток"), 
-    KeyboardButton("Витрати")) 
-    keyboard.add(KeyboardButton("Мій баланс"), 
-    KeyboardButton("Що таке wallet-bot?")) 
-return keyboard
-    
 @info_router.message(F.text == "Прибуток")
 async def request_income_info(message: Message):
     await message.answer("Будь ласка, введіть дохід у форматі: /income {сума} {опис}")
+
 
 @info_router.message(F.text == "Витрати")
 async def request_expense_info(message: Message):
@@ -38,6 +32,7 @@ async def show_balance(message: Message):
     user_id = message.from_user.id
     data_manager.load_user_data(user_id)
     await message.answer(f"Ваш поточний баланс: {data_manager.user_data[user_id]['balance']} грн.")
+
 
 @info_router.message(F.text == "/help")
 async def show_balance(message: Message):
@@ -51,6 +46,7 @@ async def show_balance(message: Message):
         f"/history - Переглянути історію транзакцій"
     )
     await message.answer(help_message)
+  
     
 @info_router.message(F.text == "Що таке wallet-bot?")
 async def about_command(message: Message):
