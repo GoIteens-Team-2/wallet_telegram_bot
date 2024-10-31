578a3abcaa96a56067535162616a78d73d658387
Риманов Ростислав <r.rostislav2019@gmail.com>
2024-10-31 16:00:42 +0200

    Update info.py
    Додав кнопки

--- src/handlers/info.py
+++ src/handlers/info.py
@@ -1,6 +1,7 @@
-from aiogram import Router, F
+from aiogram import Router
+from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 from src.service.data_management import data_manager
 
 
@@ -32,7 +33,7 @@ async def show_balance(message: Message):
 
 
 @info_router.message(Command("help"))
-async def show_balance(message: Message):
+async def show_help(message: Message):
     user_id = message.from_user.id
     data_manager.load_user_data(user_id)
     help_message = (
@@ -47,5 +48,38 @@ async def show_balance(message: Message):
     )
     await message.answer(help_message)
 
+ def get_history_keyboard():
+    keyboard = InlineKeyboardBuilder()
+    keyboard.row(
+        InlineKeyboardButton(text="Історія доходів", callback_data="history:incomes"),
+        InlineKeyboardButton(text="Історія витрат", callback_data="history:expenses"),
+    )
+    keyboard.row(
+        InlineKeyboardButton(text="Транзакції від дати", callback_data="history:from_date"),
+        InlineKeyboardButton(text="Баланс", callback_data="history:balance"),
+    )
+    return keyboard.as_markup()
+
 @info_router.message(Command("history"))
-async def 
+async def command_history(message: Message):
+    await message.answer("Історії транзакцій:", reply_markup=get_history_keyboard())
+
+@info_router.callback_query(lambda c: c.data == "history:incomes")
+async def history_incomes(callback_query):
+    await callback_query.message.answer("Команда: /historyIncomes")
+    await callback_query.answer()
+
+@info_router.callback_query(lambda c: c.data == "history:expenses")
+async def history_expenses(callback_query):
+    await callback_query.message.answer("Команда: /historyExpenses")
+    await callback_query.answer()
+
+@info_router.callback_query(lambda c: c.data == "history:from_date")
+async def history_from_date(callback_query):
+    await callback_query.message.answer("Команда: /historyFromDate")
+    await callback_query.answer()
+
+@info_router.callback_query(lambda c: c.data == "history:balance")
+async def history_balance(callback_query):
+    await callback_query.message.answer("Команда: /balance")
+    await callback_query.answer()
