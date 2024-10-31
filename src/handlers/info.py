d37a554acc6e9b652c66ed42b12cbd990800d179
Риманов Ростислав <r.rostislav2019@gmail.com>
2024-10-31 16:20:28 +0200

    Update info.py

--- src/handlers/info.py
+++ src/handlers/info.py
@@ -1,5 +1,5 @@
 from aiogram import Router
-from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import Message, CallbackQuery
 from aiogram.filters import CommandStart, Command
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from src.service.data_management import data_manager
@@ -10,7 +10,6 @@ info_router = Router()
 
 @info_router.message(CommandStart(deep_link=True))
 async def commandstart(message: Message):
-    print("Test start")
     user_id = message.from_user.id
     data_manager.load_user_data(user_id)
     welcome_message = (
@@ -64,22 +63,23 @@ async def show_help(message: Message):
 async def command_history(message: Message):
     await message.answer("Історії транзакцій:", reply_markup=get_history_keyboard())
 
+# Обробники callback_data для історії транзакцій
 @info_router.callback_query(lambda c: c.data == "history:incomes")
-async def history_incomes(callback_query):
+async def history_incomes(callback_query: CallbackQuery):
     await callback_query.message.answer("Команда: /historyIncomes")
     await callback_query.answer()
 
 @info_router.callback_query(lambda c: c.data == "history:expenses")
-async def history_expenses(callback_query):
+async def history_expenses(callback_query: CallbackQuery):
     await callback_query.message.answer("Команда: /historyExpenses")
     await callback_query.answer()
 
 @info_router.callback_query(lambda c: c.data == "history:from_date")
-async def history_from_date(callback_query):
+async def history_from_date(callback_query: CallbackQuery):
     await callback_query.message.answer("Команда: /historyFromDate")
     await callback_query.answer()
 
 @info_router.callback_query(lambda c: c.data == "history:balance")
-async def history_balance(callback_query):
+async def history_balance(callback_query: CallbackQuery):
     await callback_query.message.answer("Команда: /balance")
-    await callback_query.answer()
+    await callback_query.answer(
