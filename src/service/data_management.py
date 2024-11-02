1739528bacf27e48ef0756f4bb566418d2507a27
Likor7 <sov30082003@gmail.com>
2024-11-02 21:43:12 +0200

    Fixed data_management

--- src/service/data_management.py
+++ src/service/data_management.py
@@ -1,6 +1,7 @@
 import json
 import os
 
+
 class DataManager:
     def __init__(self) -> None:
         self.user_data = {}
@@ -10,14 +11,14 @@ class DataManager:
     def load_user_data(self, user_id):
         file_name = f"users_transactions/user_{user_id}_data.json"
         try:
-            with open(file_name, 'r', encoding="utf-8") as file:
+            with open(file_name, "r", encoding="utf-8") as file:
                 self.user_data[user_id] = json.load(file)
         except FileNotFoundError:
             self.user_data[user_id] = {"balance": 0, "transactions": []}
 
-    def load_user_data(self, user_id):
+    def save_user_data(self, user_id):
         file_name = f"users_transactions/user_{user_id}_data.json"
-        with open(file_name, 'w', encoding="utf-8") as file:
+        with open(file_name, "w", encoding="utf-8") as file:
             json.dump(self.user_data[user_id], file, indent=4)
 
 
