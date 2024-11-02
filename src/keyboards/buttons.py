99c33a3cd9cf852edb39cc3bb07c82f03d6809fe
Риманов Ростислав <r.rostislav2019@gmail.com>
2024-11-02 15:12:11 +0200

    Update buttons.py

--- src/keyboards/buttons.py
+++ src/keyboards/buttons.py
@@ -1,26 +1,22 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
- transaction_history_keyboard():
-     """Створює клавіатуру з командами для перегляду історії."""
-     return ReplyKeyboardMarkup(
+def transaction_history_keyboard():
+    """Створює клавіатуру з командами для перегляду історії."""
+    return ReplyKeyboardMarkup(
         keyboard=[
-             [KeyboardButton(text="/historyExpenses - Історія останньої витрати")],
-             [KeyboardButton(text="/historyIncomes - Історія ваших доходів")],
-           [
-                 KeyboardButton(text="/historyFromDate - Історія витрат конкретного числа")
-             ],
-            [
-                KeyboardButton(text="Головне меню", request_contact=True)
-             ]
-         ],
-         resize_keyboard=True,
-         one_time_keyboard=True
-     )
-
- async send_keyboard_example(message):
-     kb = ReplyKeyboardMarkup(
-        keyboard=[[KeyboardButton(text="Надати контакт", request_contact=True)]],
-       resize_keyboard=True,
+            [KeyboardButton(text="/historyExpenses - Історія останньої витрати")],
+            [KeyboardButton(text="/historyIncomes - Історія ваших доходів")],
+            [KeyboardButton(text="/historyFromDate - Історія витрат конкретного числа")],
+            [KeyboardButton(text="Головне меню")]
+        ],
+        resize_keyboard=True,
         one_time_keyboard=True
-     )
-     await message.answer("Будь ласка, надайте свій контакт:", reply_markup=kb)
+    )
+
+async def send_keyboard_example(message):
+    kb = ReplyKeyboardMarkup(
+        keyboard=[[KeyboardButton(text="Надати контакт", request_contact=True)]],
+        resize_keyboard=True,
+        one_time_keyboard=True
+    )
+    await message.answer("Будь ласка, надайте свій контакт:", reply_markup=kb)
