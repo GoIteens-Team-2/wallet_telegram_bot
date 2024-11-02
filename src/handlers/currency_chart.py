f9255341d29f387ea368b3a1f421b2dbbc9219eb
savenkovsemen02@gmail.com <savenkovsemen02@gmail.com>
2024-11-02 16:30:30 +0200

    upgrade code

--- src/handlers/currency_chart.py
+++ src/handlers/currency_chart.py
@@ -1,7 +1,6 @@
 from aiogram import Router
-from aiogram.types import Message
 from aiogram.filters import Command
-from aiogram.types import BufferedInputFile
+from aiogram.types import BufferedInputFile, Message
 
 from ..service.currency import get_currency_rates, create_chart
 
@@ -9,8 +8,13 @@ currency_chart_router = Router()
 
 
 @currency_chart_router.message(Command("currencyChart"))
+async def ask_currency_type(message: Message):
+    await message.answer("Введіть базову валюту для створення графіка (USD, UAH, PLN, EUR):")
+
+@currency_chart_router.message()
 async def get_currency_chart(message: Message):
+    currencys_type = message.text
     rates = get_currency_rates()
-    chart_buf = create_chart(rates)
+    chart_buf = create_chart(rates, currency_type=currencys_type)
     photo = BufferedInputFile(chart_buf.read(), filename="chart.png")
     await message.answer_photo(photo=photo)
